d447b57910b49fbbc4190ab0e9dcae723ec388f4
krav-ets <krav-ets@yandex.ru>
2018-05-14 16:57:18 +0300

    checkpoint 1: add file gendiff.js

---
+++ src/bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import program from 'commander';
+
+program
+  .version('0.0.2')
+  .description('Compares two configuration files and shows a difference')
+  .option('-f, --format [type>]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .parse(process.argv);
