5e1bcfd3a33174ae1c279cd9656f9f905f68f4a0
krav-ets <krav-ets@yandex.ru>
2018-05-18 00:02:06 +0300

    checkpoint 5

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,35 +2,45 @@ import fs from 'fs';
 import genDiff from '../src';
 
 describe('gendiff', () => {
-  it('#checkpoint 2: JSON', () => {
-    const beforeJsonPath = '__tests__/__fixtures__/cp2_before.json';
-    const afterJsonPath = '__tests__/__fixtures__/cp2_after.json';
+  it('#checkpoint 2: JSON plain', () => {
+    const beforeJsonPath = '__tests__/__fixtures__/plain_before.json';
+    const afterJsonPath = '__tests__/__fixtures__/plain_after.json';
 
     const diffString = genDiff(beforeJsonPath, afterJsonPath);
 
-    const expected = fs.readFileSync('__tests__/__fixtures__/cp2_expected', 'utf-8');
+    const expected = fs.readFileSync('__tests__/__fixtures__/plain_expected', 'utf-8');
 
     expect(diffString).toEqual(expected);
   });
 
-  it('#checkpoint 3: YAML', () => {
-    const beforeJsonPath = '__tests__/__fixtures__/cp3_before.yaml';
-    const afterJsonPath = '__tests__/__fixtures__/cp3_after.yaml';
+  it('#checkpoint 3: YAML plain', () => {
+    const beforeJsonPath = '__tests__/__fixtures__/plain_before.yaml';
+    const afterJsonPath = '__tests__/__fixtures__/plain_after.yaml';
 
     const diffString = genDiff(beforeJsonPath, afterJsonPath);
 
-    const expected = fs.readFileSync('__tests__/__fixtures__/cp2_expected', 'utf-8');
+    const expected = fs.readFileSync('__tests__/__fixtures__/plain_expected', 'utf-8');
 
     expect(diffString).toEqual(expected);
   });
 
-  it('#checkpoint 4: ini', () => {
-    const beforeJsonPath = '__tests__/__fixtures__/cp4_before.ini';
-    const afterJsonPath = '__tests__/__fixtures__/cp4_after.ini';
+  it('#checkpoint 4: ini plain', () => {
+    const beforeJsonPath = '__tests__/__fixtures__/plain_before.ini';
+    const afterJsonPath = '__tests__/__fixtures__/plain_after.ini';
 
     const diffString = genDiff(beforeJsonPath, afterJsonPath);
 
-    const expected = fs.readFileSync('__tests__/__fixtures__/cp2_expected', 'utf-8');
+    const expected = fs.readFileSync('__tests__/__fixtures__/plain_expected', 'utf-8');
+    expect(diffString).toEqual(expected);
+  });
+
+  it('#checkpoint 5: JSON tree', () => {
+    const beforeJsonPath = '__tests__/__fixtures__/before.json';
+    const afterJsonPath = '__tests__/__fixtures__/after.json';
+
+    const diffString = genDiff(beforeJsonPath, afterJsonPath);
+
+    const expected = fs.readFileSync('__tests__/__fixtures__/expected', 'utf-8');
     expect(diffString).toEqual(expected);
   });
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,82 @@ import _ from 'lodash';
 import path from 'path';
 import getParser from './parsers';
 
+const genAst = (before, after) => {
+  const keys = _.union(_.keys(before), _.keys(after));
+  return keys.map((key) => {
+    if (_.isPlainObject(before[key]) && _.isPlainObject(after[key])) {
+      return {
+        key,
+        type: 'nested',
+        children: genAst(before[key], after[key]),
+      };
+    }
+    if (before[key] === after[key]) {
+      return {
+        key,
+        type: 'unchanged',
+        beforeValue: before[key],
+      };
+    }
+    if (!_.has(before, key)) {
+      return {
+        key,
+        type: 'added',
+        afterValue: after[key],
+      };
+    }
+    if (!_.has(after, key)) {
+      return {
+        key,
+        type: 'deleted',
+        beforeValue: before[key],
+      };
+    }
+    return {
+      key,
+      type: 'updated',
+      beforeValue: before[key],
+      afterValue: after[key],
+    };
+  });
+};
+
+const render = (data) => {
+  const genString = (ast, n) => {
+    const interval = num => ' '.repeat(num);
+
+    const stringify = (val) => {
+      if (_.isPlainObject(val)) {
+        const keys = Object.keys(val);
+        const printedVal = keys.map(key => `${interval(n + 6)}${key}: ${val[key]}`);
+        return `{\n${printedVal.join('\n')}\n${interval(n + 2)}}`;
+      }
+      return val;
+    };
+
+    const objToStr = ast.map((obj) => {
+      switch (obj.type) {
+        case 'nested':
+          return `${interval(n + 2)}${obj.key}: {\n${genString(obj.children, n + 4)}\n${interval(n + 2)}}`;
+        case 'updated':
+          return `${interval(n)}+ ${obj.key}: ${stringify(obj.afterValue)}\n${interval(n)}- ${obj.key}: ${stringify(obj.beforeValue)}`;
+        case 'unchanged':
+          return `${interval(n + 2)}${obj.key}: ${stringify(obj.beforeValue)}`;
+        case 'added':
+          return `${interval(n)}+ ${obj.key}: ${stringify(obj.afterValue)}`;
+        case 'deleted':
+          return `${interval(n)}- ${obj.key}: ${stringify(obj.beforeValue)}`;
+        default:
+          return null;
+      }
+    });
+
+    return objToStr.join('\n');
+  };
+
+  return `{\n${genString(data, 2)}\n}`;
+};
+
 const genDiff = (beforePath, afterPath) => {
   const ext = path.extname(beforePath);
   const beforeData = fs.readFileSync(beforePath, 'utf-8');
@@ -10,20 +86,8 @@ const genDiff = (beforePath, afterPath) => {
   const parse = getParser(ext);
   const beforeObject = parse(beforeData);
   const afterObject = parse(afterData);
-
-  const keys = _.union(_.keys(beforeObject), _.keys(afterObject));
-
-  return `{\n${keys.map((key) => {
-    if (_.has(beforeObject, key) && _.has(afterObject, key)) {
-      if (beforeObject[key] === afterObject[key]) {
-        return `    ${key}: ${beforeObject[key]}`;
-      }
-      return `  + ${key}: ${afterObject[key]}\n  - ${key}: ${beforeObject[key]}`;
-    } else if (!_.has(beforeObject, key)) {
-      return `  + ${key}: ${afterObject[key]}`;
-    }
-    return `  - ${key}: ${beforeObject[key]}`;
-  }).join('\n')}\n}`;
+  const ast = genAst(beforeObject, afterObject);
+  return render(ast);
 };
 
 export default genDiff;
